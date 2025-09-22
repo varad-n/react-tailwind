44ee1cc13450788017b9cea5c8ffbecea01edba5
varad-n <hvi924926@gmail.com>
2025-09-22 23:28:33 +0530

    in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,6 +10,22 @@ const Navbar = ({theme, setTheme}) => {
       <img src={theme === 'dark' ? assets.logo_dark : assets.logo } 
       className='w-32 sm:w-40' alt='' />
 
+      <div className='text-gray-700 dark:text-white sm:text-sm max-sm:w-60 max-sm:pl-10 
+      max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col 
+      max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all'>
+        <a href='#' className='sm:hover:border-b'>Home</a>
+        <a href='#services' className='sm:hover:border-b'>Services</a>
+        <a href='#our-work' className='sm:hover:border-b'>Our Work</a>
+        <a href='#contact-us' className='sm:hover:border-b'>Contact Us</a>
+      </div>
+
+      <div>
+        <a href='#contact-us' className='text-sm max-sm:hidden flex items-center gap-2 bg-primary 
+        text-white px-6 py-2 rounded-full cursor-pointer hover:scale-103 transition-all'>
+          Connect <img src={assets.arrow_icon} width={14} alt=''/>
+        </a>
+      </div>
+
     </div>
   )
 }
