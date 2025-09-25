686fade4d4d0f6a2e1ba43b06a3cfd101de12bde
varad-n <hvi924926@gmail.com>
2025-09-26 00:59:02 +0530

    Services section with scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
+import Services from './components/Services'
 
 const App = () => {
 
@@ -10,6 +12,8 @@ const App = () => {
     <div className='dark:bg-black relative'>
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
+      <TrustedBy />
+      <Services />
     </div>
   )
 }
---
+++ src/components/Services.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import assets from '../assets/assets'
+import Title from './title'
+
+const Services = () => {
+
+    const servicesData = [
+        {
+            title: 'Advertising',
+            description: 'We turn bold ideas inot powerful digital solutions that connect, engage...',
+            icon: assets.ads_icon
+        },
+        {
+            title: 'Content Marketing',
+            description: 'We help you execute your plan and deliver results.',
+            icon: assets.marketing_icon
+        },
+        {
+            title: 'Content Writing',
+            description: 'We help you create a marketing strategy that drives results.',
+            icon: assets.content_icon
+        },
+        {
+            title: 'Social Media',
+            description: 'We help you build a strong social media presence and engage with your audience.',
+            icon: assets.social_icon
+        }
+    ]
+  return (
+    <div id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
+        <img src={assets.bgImage2} alt='' className='absolute -top-110 -left-70 -z-1 dark:hidden' />
+
+    <Title title='How can we help?' desc='From strategy to execution, we craft digital solutions taht move your business forward.'/>
+    </div>
+  )
+}
+
+export default Services
