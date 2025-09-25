c827bedaa01d712f5d9d60f3975d11919454ff42
varad-n <hvi924926@gmail.com>
2025-09-26 00:21:55 +0530

    gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,14 @@ const Hero = () => {
         xl:leading-[95px] max-w-5xl'>Turning Imagination into <span className='bg-gradient-to-r from-[#5044E5] to-[#4d8cea] 
         bg-clip-text text-transparent'>Digital</span> Impact.</h1>
 
-        <p>Creating meaningful connection and turning big ideas into interactive digital experiences.</p>
+        <p className='text-sm sm:text-lg font-medium text-gray-500 dark:text-white/75 
+        max-w-4/5 sm:max-w-lg pb-3'>Creating meaningful connection and turning big ideas into interactive digital experiences.</p>
 
+        <div>
+          <img src={assets.hero_img} alt='' className='w-full max-w-6xl'/>
+          <img src={assets.bgImage1} alt='' className='absolute -top-40 -right-40 sm:top-2 sm:-right-70 -z-1 dark-hidden' />
+        </div>
+        
     </div>
   )
 }
