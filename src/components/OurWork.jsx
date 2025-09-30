c7d5c7d085b7191b8d32ba30f6aac2fc92841a4a
varad-n <hvi924926@gmail.com>
2025-10-01 00:34:05 +0530

    bug

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Title from './title'
 
-const OutWork = () => {
+const OurWork = () => {
   return (
     <div id='our-work' className='flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 
     pt-30 text-gray-700 dark:text-white'>
