0dbc34eb8c30ded7b236a6d57647578ff6d62631
varad-n <hvi924926@gmail.com>
2025-10-01 19:17:40 +0530

    form added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero'
 import TrustedBy from './components/TrustedBy'
 import Services from './components/Services'
 import OurWork from './components/OurWork'
+import Teams from './components/Teams'
+import ContactUs from './components/ContactUs'
 
 const App = () => {
 
@@ -16,6 +18,8 @@ const App = () => {
       <TrustedBy />
       <Services />
       <OurWork />
+      <Teams />
+      <ContactUs />
     </div>
   )
 }
