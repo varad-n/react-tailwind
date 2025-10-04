a72b3ba559138d535dfa4214140392aaa392bc3b
varad-n <hvi924926@gmail.com>
2025-10-04 12:20:43 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Services from './components/Services'
 import OurWork from './components/OurWork'
 import Teams from './components/Teams'
 import ContactUs from './components/ContactUs'
+import Footer from './components/Footer'
 
 const App = () => {
 
@@ -20,6 +21,7 @@ const App = () => {
       <OurWork />
       <Teams />
       <ContactUs />
+      <Footer />
     </div>
   )
 }
